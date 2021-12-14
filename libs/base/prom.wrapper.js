f26f29aa0c155bdeb59679daae7526411a0551b7
wangye6104 <87463912@qq.com>
2021-12-14 13:59:04 +0800

    Refactor prom.wrapper.js and add http-monitor

--- libs/base/prom.wrapper.js
+++ libs/base/prom.wrapper.js
@@ -6,36 +6,41 @@ const os = require('os');
 const client = require('prom-client');
 const pubdefs = require('../../include/sysdefs');
 
-const Registry = client.Registry;
-const register = new Registry();
-register.setDefaultLabels({ instance: os.hostname() });
-const prefix = process.env.PROM_PREFIX || 'rdf4_';
+const register = client.register;
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics({ prefix, register });
+collectDefaultMetrics({
+    register: register
+});
 
-function _regCounter(name, help) {
+const defaultLabels = {
+    service: theApp.getRegistry(),
+    instance: theApp.getInstance()
+};
+register.setDefaultLabels(defaultLabels);
+
+function _regCounter(name, help, labelNames) {
     return new client.Counter({
         name: name,
         help: help,
-        registers: [register]
+        labelNames: labelNames
     });
 }
 
-function _regGauge(name, help) {
+function _regGauge(name, help, labelNames) {
     return new client.Gauge({
         name: name,
         help: help,
-        registers: [register]
+        labelNames: labelNames
     });
 }
 
-function _regGaugeAsync(name, help, fnCollectAsync) {
+function _regGaugeAsync(name, help, labelNames, fnCollectAsync) {
     assert(typeof fnCollectAsync === 'function');
     new client.Gauge({
         name: name,
         help: help,
-        registers: [register],
+        labelNames: labelNames,
         async collect() {
             const d = await fnCollectAsync();
             this.set(d)
@@ -49,13 +54,14 @@ exports.regMetrics = (options) => {
     options.metrics.forEach(metric => {
         let name = `${prefix}_${metric.name}`
         let help = metric.help || `${name}_help`;
+        let labelNames = metric.labelNames || [];
         if (metric.type === pubdefs.eMetricType.COUNTER) {
-            collectors[metric.name] = _regCounter(name, help);
+            collectors[metric.name] = _regCounter(name, help, labelNames);
         } else if (metric.type === pubdefs.eMetricType.GAUGE) {
             if (metric.fnCollectAsync) {
-                _regGaugeAsync(name, help, metric.fnCollectAsync);
+                _regGaugeAsync(name, help, labelNames, metric.fnCollectAsync);
             } else {
-                collectors[metric.name] = _regGauge(name, help);
+                collectors[metric.name] = _regGauge(name, help, labelNames);
             }
         }
     });
---
+++ libs/common/http-monitor.js
@@ -0,0 +1,39 @@
+/**
+ * Create by eric on 2021/12/14
+ */
+ const pubdefs = require("../../include/sysdefs");
+ const mntService = require('../base/prom.wrapper');
+ 
+ const gIgnoredUrls = [
+     '/monitor/health',
+     '/monitor/metrics'
+ ];
+ 
+ const eHttpMetrics = {
+     REQ_TOTAL : 'requests_received_total',
+     REQ_CODE_TOTAL: 'requests_status_code_total'
+ };
+ 
+ let metricsCollector = mntService.regMetrics({
+     moduleName: 'http',
+     metrics: [{
+         name: eHttpMetrics.REQ_TOTAL,
+         type: pubdefs.eMetricType.COUNTER,
+         labelNames: ['url']
+     }, {
+         name: eHttpMetrics.REQ_CODE_TOTAL,
+         type: pubdefs.eMetricType.COUNTER,
+         labelNames: ['url', 'status_code']
+     }]
+ });
+ 
+ module.exports = exports = (req, res, next) => {
+     res.on('finish', () => {
+         let originalUrl = req.originalUrl || req.url;
+         if (gIgnoredUrls.indexOf(originalUrl) === -1) {
+             metricsCollector[eHttpMetrics.REQ_TOTAL].inc({url: originalUrl}, 1)
+             metricsCollector[eHttpMetrics.REQ_CODE_TOTAL].inc({url: originalUrl, 'status_code': res.statusCode}, 1)
+         }
+     });
+     next();
+ };
