f94af71e8f76778de74c8169c8afcde7c8fc4d2a
wangye6104 <87463912@qq.com>
2021-12-27 19:04:06 +0800

    Optimize redis.wrapper

--- libs/common/redis.wrapper.js
+++ libs/common/redis.wrapper.js
@@ -1,14 +1,23 @@
 /**
  * Create by Eric on 2021/12/24
  */
-const redis = require('redis');
-const {redis: config} = require('../conf/config');
+const {createClient} = require('redis');
 const theApp = require('../../bootstrap');
+const {redis: config} = require('../base/config');
 const mntService = require('../base/prom.wrapper');
 const {WinstonLogger} = require('../base/winston.wrapper');
 const logger = WinstonLogger(process.env.SRV_ROLE || 'redis');
 
 const MODULE_NAME = 'REDIS_CONN';
+function _onConnectionEnd(clientId, parent) {
+    logger.info(`${this.name}: Redis connection closed. - ${clientId}.`);
+    let client = this._clients[clientId];
+    if (client !== undefined) {
+        client.close();
+        delete this._clients[clientId];
+    }
+}
+
 let redisWrapper = {
     name: MODULE_NAME,
     _clients : {},
@@ -21,28 +30,26 @@ let redisWrapper = {
         let db = options.db === undefined? 0 : options.db;
         let mode = options.mode === undefined? 'pub' : options.mode;
         let clientId = prefix + db + mode;
-        if (clients[clientId] !== undefined) {
-            return clients[clientId];
+        if (this._clients[clientId] !== undefined) {
+            return this._clients[clientId];
         }
         let clientCfg = Object.assign(config.connection, {
             prefix: prefix,
             db: db
         });
-        let client = redis.createClient(clientCfg);
-        client.on('error', function(err) {
-            console.log(err.code, err.message);
+        let client = createClient(clientCfg);
+        client.on('error', (err) => {
+            logger.error(`${this.name}: Create client error! - ${err.message}`);
+            return null;
         });
-        client.on('ready', function() {
-            console.log('Redis connected.');
+        client.on('ready', () => {
+            this._clients[clientId] = client;
+            logger.info(`${this.name}: redis connected.`);
+            return client;
         });
-        client.on('end', () => {
-            console.log('Redis connection closed.');
-        });
-        clients[clientId] = client;
-        return client;
+        client.on('end', _onConnectionEnd.bind(this, clientId, options.parent));
     }
 }
-
-theApp.regModule()
+theApp.regModule(redisWrapper);
 
 module.exports = redisWrapper;
