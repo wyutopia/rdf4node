57c110351e1b0adc690394e513120ed6a2714fad
wangye6104 <87463912@qq.com>
2022-03-14 18:38:35 +0800

    Add pack script into package.json. Add router.js into libs/base

---
+++ templates/routes/monitor.js
@@ -0,0 +1,16 @@
+/**
+ * Create by eric 2021/06/17
+ */
+ const {monitor} = require('@icedeer/rdf4node');
+
+ module.exports = exports = [
+   {
+     path: 'metrics',
+     method: 'get',
+     handler: monitor.getMetrics
+   }, {
+     path: 'health',
+     method: 'get',
+     handler: monitor.checkHealth
+   }
+ ];
