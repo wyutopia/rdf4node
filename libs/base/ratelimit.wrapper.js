a9fdcd4a52124d33d85457134fa7ff60343922e2
wangye6104 <87463912@qq.com>
2022-04-06 09:52:12 +0800

    Upgrade express-rate-limit lib

--- libs/base/ratelimit.wrapper.js
+++ libs/base/ratelimit.wrapper.js
@@ -1,21 +1,29 @@
 /**
  * Created by Eric on 2021/11/15
+ * Modified by Eric on 2022/04/06
  */
- const RateLimit = require('express-rate-limit');
- const MongoStore = require('rate-limit-mongo');
- const {mongodb, rateLimit: config} = require('./config');
- 
- const dbUri = `mongodb://${mongodb.user}:${encodeURIComponent(mongodb.pwd)}` 
-                + `@${mongodb.ip}:${mongodb.port || 27017}/${mongodb.db}?authSource=${mongodb.authSource || mongodb.db}`;
- const limiter = new RateLimit({
-     store: new MongoStore({
-         uri: dbUri
-     }),
-     collectionName: 'accessRateLimit',
-     windowMs: config.windowMs || (15 * 60 * 1000),
-     max: config.max || 100,
-     expireTimeMs: config.expireTimeMs || (60 * 1000)
- });
- 
- module.exports = limiter;
- 
+const rateLimit = require('express-rate-limit');
+const MongoStore = require('rate-limit-mongo');
+const { mongodb, rateLimit: rateLimitConf } = require('./config');
+
+const config = Object.assign({}, rateLimitConf.options);
+if (config.windowMs === undefined) {
+    config.windowMs = 15 * 60 * 1000;
+}
+if (config.max === undefined) {
+    config.max = 20;
+}
+if (config.expireTimeMs === undefined) {
+    config.expireTimeMs = 60 * 1000;
+}
+if (rateLimitConf.store === 'mongo') {
+    let dbUri = `mongodb://${mongodb.user}:${encodeURIComponent(mongodb.pwd)}`
+        + `@${mongodb.ip}:${mongodb.port || 27017}/${mongodb.db}?authSource=${mongodb.authSource || mongodb.db}`;
+    config.store = new MongoStore({
+        uri: dbUri,
+        collectionName: 'accessRateLimit'
+    });
+}
+
+const limiter = rateLimit(config);
+module.exports = limiter;
