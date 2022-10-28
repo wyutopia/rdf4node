3458f03dee087f05abde72614d639bf56253d064
Eric Wang <wyutopia@hotmail.com>
2022-10-28 10:45:17 +0800

    Adjust route.js

--- libs/base/router.js
+++ libs/base/router.js
@@ -99,7 +99,7 @@ function _loadRouteConfig(pathPrefix, dir, filename) {
         _loadRoutes();
         //
         gRoutes.forEach(route => {
-            logger.info(`Set system route: ${route.path} - ${route.method} - ${route.authType || 'na'}`);
+            logger.info(`Set system route: ${route.path} - ${route.method} - ${route.authType}`);
             let method = (route.method || 'USE').toLowerCase();
             if (route.authType === 'none') {
                 router[method](route.path, route.handler);
