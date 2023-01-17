bf2a5364d42dd191c3d81d127aa6fad52deef67c
Eric Wang <wyutopia@hotmail.com>
2023-01-17 19:21:40 +0800

    Add sysevents

---
+++ include/sysevents.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Eric on 2023/01/17
+ */
+module.exports = exports = {
+    SYS_MODULE_INIT            : '_sys_module_init',
+    SYS_MODULE_START           : '_sys_module_start',
+    SYS_MODULE_STOP            : '_sys_module_stop'
+};
