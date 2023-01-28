7eba11318b72b325a58277f30506528073e85b06
Eric Wang <wyutopia@hotmail.com>
2023-01-28 13:38:15 +0800

    Add system events definition

---
+++ include/sys-events.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Eric on 2023/01/17
+ */
+module.exports = exports = {
+    // Module
+    SYS_MODULE_CREATE              : 'sys.module.create',
+    SYS_MODULE_INIT                : 'sys.module.init',
+    SYS_MODULE_ACTIVE              : 'sys.module.active',
+    SYS_MODULE_HALT                : 'sys.module.halt',
+    SYS_MODULE_RESUME              : 'sys.module.resume',
+    // Admin
+    SYS_ADMIN_CREATE               : 'sys.admin.create',
+    SYS_ADMIN_UPDATE               : 'sys.admin.update',
+    SYS_ADMIN_CHGPWD               : 'sys.admin.chgpwd',
+    SYS_ADMIN_SUSPEND              : 'sys.admin.suspend',
+    SYS_ADMIN_DELETE               : 'sys.admin.delete',
+    // License
+    SYS_LIC_CREATE                 : 'sys.lic.create',
+    SYS_LIC_UPDATE                 : 'sys.lic.update',
+    SYS_LIC_DELETE                 : 'sys.lic.delete',
+    // Message
+    MSG_CREATE                     : 'msg.create',
+    MSG_UPDATE                     : 'msg.update',
+    MSG_DELETE                     : 'msg.delete',
+    MSG_READ                       : 'msg.read'
+    // Customized application events go here ...
+};
--- include/sysevents.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Eric on 2023/01/17
- */
-module.exports = exports = {
-    SYS_MODULE_INIT            : '_sys_module_init',
-    SYS_MODULE_START           : '_sys_module_start',
-    SYS_MODULE_STOP            : '_sys_module_stop'
-};
