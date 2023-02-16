42dedd0b696f34ffbd828f139b21a482566f7e55
Eric Wang <wyutopia@hotmail.com>
2023-02-16 14:55:04 +0800

    Refactor EntityController for better usage

--- framework/components.js
+++ framework/components.js
@@ -133,48 +133,90 @@ const _defaultCtlSpec = {
     mandatoryAddKeys: {},
     populateKeys: [],
     selectKeys: null,
-    // For customized query operations
-    extFindFilter: _$extFindFilter,
-    parsePatch: _$parsePatch,
-    extUpdates: _$extUpdates,
-    allowDelete : _$allowDelete,
-    extDeleteFilter: _$extDeleteFilter
+    // For overridable query operations
+    beforeFindAll: tools.noop,
+    beforeFindByProject: tools.noop,
+    beforeFindByUser: tools.noop,
+    afterFindAll: tools.noop,
+    afterFindOne: tools.noop,
+    afterFindPartial: tools.noop,
+    //
+    beforeAdd: function () { return {} },
+    afterAdd: tools.noop,
+    //
+    beforeUpdateOne: tools.noop,
+    afterUpdateOne: tools.noop,
+    //
+    allowDelete: function (id, callback) { return callback('Not allowed!', false) },
+    beforeDeleteOne: tools.noop
 };
-
-function _initControlSpec(ctlSpec) {
+function _initCtlSpec(ctlSpec) {
     Object.keys(_defaultCtlSpec).forEach( key => {
         let privateKey = `_${key}`;
         this[privateKey] = ctlSpec[key] || _defaultCtlSpec[key];
     });
 }
 
+function _beforeFind(args) {
+    let options = {
+        filter: tools.deepAssign({}, args),
+        populate: this._populateKeys,
+    };
+    if (this._selectKeys) {
+        options.select = this._selectKeys;
+    }
+    return options;
+}
+
+function _beforeUpdate(args) {
+    let setData = tools.deepAssign({}, args);
+    delete setData.id;
+    if (Object.keys(setData).length === 0) {
+        return {noop: true};
+    }
+    setData.updateAt = new Date();
+    let options = {
+        filter: {
+            _id: args.id
+        },
+        updates: {
+            $set: setData
+        },
+        populate: this._populateKeys
+    }
+    if (this._selectKeys) {
+        options.select = this._selectKeys;
+    }
+    return options;
+}
+
+function _beforePatch(args) {
+    let updates = this._parsePatch(args.jsonPatch);
+    //
+    if (Object.keys(updates).length === 0) {
+        return {noop: 'Empty updates!'}
+    }
+    //
+    let options = {
+        filter: {_id: args.id},
+        updates: updates
+    }
+    if (this._selectKeys) {
+        options.select = this._selectKeys;
+    }
+    return options;
+}
+
+// The class
 class EntityController extends ControllerBase {
     constructor(props) {
         super(props);
-        // 
-        this.modelSpec = props.modelSpec;
-        // Declaring the repoSpec of the entity
+        // Init repo properties
         this.modelName = props.modelName || 'test';
         this.modelSchema = props.modelSchema || {};
         this.dsName = props.dsName || 'default';
-        // Declaring the ctlSpec of the entity
-        this._searchKeys = props.searchKeys || {};
-        this._propKeys = props.propKeys || {};
-        this._mandatoryAddKeys = props.mandatoryAddKeys || [];
-        this._mutableKeys = props.mutableKeys || this._propKeys;
-        this._populateKeys = props.populateKeys || [];
-        this._selectKeys = props.selectKeys || null;
-        // Controller methods
-        this._extFindFilter = props.extFindFilter || _$extFindFilter;
-        this._extUpdates = props.extUpdates || _$extUpdates;
-        this._parsePatch = props.parsePatch || _$parsePatch;
-        this._allowDelete = props.allowDelete || _$allowDelete;
-        this._extDeleteFilter = props.extDeleteFilter || _$extDeleteFilter;
-        
-        if (props.ctlSpec !== undefined) {
-            _initControlSpec.call(this, props.ctlSpec);
-        }
-        
+        // Init controller properties
+        _initCtlSpec.call(this, props.ctlSpec || {});
         // Implementing the class methods
         this._getRepo = (options, callback) => {
             assert(options !== undefined);
@@ -197,40 +239,61 @@ class EntityController extends ControllerBase {
         // Register event publishers
         this._domainEvents = props.domainEvents || {};
         // Implementing basic CRUD methods
-        this.listAll = (req, res) => {
+        this.findOne = (req, res) => {
+            let params = Object.assign({}, req.params, req.query, req.body);
+            tools.parseParameter2(params, {
+                id: {
+                    type: 'ObjectId',
+                    required: true
+                }
+            }, (err, args) => {
+                if (err) {
+                    return res.sendRsp(err.code, err.message);
+                }
+                this._getRepo(req.dataSource, (err, repo) => {
+                    if (err) {
+                        return res.sendRsp(err.code, err.message);
+                    }
+                    let options = _beforeFind.call(this, args);
+                    repo.findOne(options, (err, doc) => {
+                        if (err) {
+                            return res.sendRsp(err.code, err.message);
+                        }
+                        this._afterFindOne(doc);
+                        return res.sendSuccess(doc);
+                    });
+                });
+            });
+        };
+        this.findAll = (req, res) => {
             let validator = Object.assign({}, repoFactory.paginationKeys, this._searchKeys);
             tools.parseParameter2(req.query, validator, (err, args) => {
                 if (err) {
                     return res.sendRsp(err.code, err.message);
                 }
-                let filter = this._extFindFilter(args);
-                //
                 this._getRepo(req.dataSource, (err, repo) => {
                     if (err) {
                         return res.sendRsp(err.code, err.message);
                     }
-                    let options = {
-                        filter: filter,
-                        populate: this._populateKeys
-                    };
-                    if (this._selectKeys) {
-                        options.select = this._selectKeys;
-                    }
+                    //
+                    let options = _beforeFind.call(this, args);
+                    this._beforeFindAll(options);
                     repo.findPartial(options, (err, results) => {
                         if (err) {
                             return res.sendRsp(err.code, err.message);
                         }
                         _publishEvents.call(this, {
-                            method: 'listAll',
+                            method: 'findAll',
                             data: results
                         }, () => {
+                            this._afterFindMany(results);
                             return res.sendSuccess(results);
                         });
                     });
                 });
             });
         };
-        this.listByProject = (req, res) => {
+        this.findByProject = (req, res) => {
             let params = Object.assign({}, req.params, req.query);
             let validator = Object.assign({
                 id: {
@@ -243,33 +306,28 @@ class EntityController extends ControllerBase {
                 if (err) {
                     return res.sendRsp(err.code, err.message);
                 }
-                //
                 this._getRepo(req.dataSource, (err, repo) => {
                     if (err) {
                         return res.sendRsp(err.code, err.message);
                     }
-                    let options = {
-                        filter: args,
-                        populate: this._populateKeys
-                    };
-                    if (this._selectKeys) {
-                        options.select = this._selectKeys;
-                    }
+                    let options = _beforeFind.call(this, args);
+                    this._beforeFindByProject(options);
                     repo.findMany(options, (err, docs) => {
                         if (err) {
                             return res.sendRsp(err.code, err.message);
                         }
                         _publishEvents.call(this, {
-                            method: 'listByProject',
+                            method: 'findByProject',
                             data: docs
                         }, () => {
+                            this._afterFindMany(docs);
                             return res.sendSuccess(docs);
                         });
                     });
                 });
             });
         };
-        this.listByUser = (req, res) => {
+        this.findByUser = (req, res) => {
             let params = Object.assign({}, req.params, req.query);
             let validator = Object.assign({
                 id: {
@@ -286,19 +344,14 @@ class EntityController extends ControllerBase {
                     if (err) {
                         return res.sendRsp(err.code, err.message);
                     }
-                    let options = {
-                        filter: args,
-                        populate: this._populateKeys
-                    };
-                    this.emit('BeforeListByUser', options, args);
-                    if (this._selectKeys) {
-                        options.select = this._selectKeys;
-                    }
+                    //
+                    let options = _beforeFind.call(this, args);
+                    this._beforeFindByUser(options);
                     repo.findMany(options, (err, docs) => {
                         if (err) {
                             return res.sendRsp(err.code, err.message);
                         }
-                        this.emit('AfterListByUser', docs);
+                        this._afterFindMany(docs);
                         return res.sendSuccess(docs);
                     });
                 });
@@ -320,8 +373,7 @@ class EntityController extends ControllerBase {
                     if (err) {
                         return res.sendRsp(err.code, err.message);
                     }
-                    let doc = tools.deepAssign({}, args);
-                    this.emit('BeforeAddOne', doc, args);
+                    let doc = this._beforeAdd(args, repo);
                     repo.create(doc, (err, result) => {
                         if (err) {
                             return res.sendRsp(err.code, err.message);
@@ -330,40 +382,9 @@ class EntityController extends ControllerBase {
                             method: 'addOne',
                             data: result
                         }, () => {
-                            this.emit('AfterAddOne', result);
+                            this._afterAdd(result);
                             return res.sendSuccess(result);
                         });
-                    });
-                });
-            });
-        };
-        this.fetchOne = (req, res) => {
-            let params = Object.assign({}, req.params, req.query, req.body);
-            tools.parseParameter2(params, {
-                id: {
-                    type: 'ObjectId',
-                    required: true
-                }
-            }, (err, args) => {
-                if (err) {
-                    return res.sendRsp(err.code, err.message);
-                }
-                //
-                this._getRepo(req.dataSource, (err, repo) => {
-                    if (err) {
-                        return res.sendRsp(err.code, err.message);
-                    }
-                    let options = {
-                        populate: this._populateKeys
-                    }
-                    if (this._selectKeys) {
-                        options.select = this._selectKeys;
-                    }
-                    repo.findById(args.id, options, (err, doc) => {
-                        if (err) {
-                            return res.sendRsp(err.code, err.message);
-                        }
-                        return res.sendSuccess(doc);
                     });
                 });
             });
@@ -384,23 +405,11 @@ class EntityController extends ControllerBase {
                     if (err) {
                         return res.sendRsp(err.code, err.message);
                     }
-                    let setData = tools.deepAssign({}, args);
-                    delete setData.id;
-                    if (Object.keys(setData).length === 0) {
-                        return res.sendRsp(eRetCodes.ACCEPTED, 'Empty updates!');
+                    let options = _beforeUpdate.call(this, args);
+                    if (options.noop) {
+                        return res.sendRep(eRetCodes.ACCEPTED, 'Empty updates!');
                     }
-                    setData.updateAt = new Date();
-                    let options = {
-                        filter: {
-                            _id: args.id
-                        },
-                        updates: this._extUpdates(setData),
-                        populate: this._populateKeys
-                    }
-                    if (this._selectKeys) {
-                        options.select = this._selectKeys;
-                    }
-                    this.emit('BeforeUpdateOne', options, args);
+                    this._beforeUpdateOne(options);
                     repo.updateOne(options, (err, doc) => {
                         if (err) {
                             return res.sendRsp(err.code, err.message);
@@ -409,7 +418,7 @@ class EntityController extends ControllerBase {
                             method: 'updateOne',
                             data: doc
                         }, () => {
-                            this.emit('AfterUpdateOne', doc);
+                            this._afterUpdateOne(doc);
                             return res.sendSuccess(doc);
                         });
                     });
@@ -436,10 +445,9 @@ class EntityController extends ControllerBase {
                             return res.sendRsp(eRetCodes.DB_DELETE_ERR, reason);
                         }
                         //
-                        let filter = _packDeleteFilter.call(this, args);
-                        repo.remove({
-                            filter: filter
-                        }, (err, result) => {
+                        let options = _beforeDelete.call(this, args);
+                        this._beforeDeleteOne(options);
+                        repo.remove(options, (err, result) => {
                             if (err) {
                                 return res.sendRsp(err.code, err.message);
                             }
@@ -472,20 +480,10 @@ class EntityController extends ControllerBase {
                     if (err) {
                         return res.sendRsp(err.code, err.message);
                     }
-                    let updates = this._parsePatch(args.jsonPatch);
-                    //
-                    if (Object.keys(updates).length === 0) {
-                        return res.sendRsp(eRetCodes.ACCEPTED, 'Invalid jsonPatch!');
+                    let options = _beforePatch.call(this, args);
+                    if (options.noop) {
+                        return res.sendRsp(eRetCodes.ACCEPTED, options.noop);
                     }
-                    //
-                    let options = {
-                        filter: {_id: args.id},
-                        updates: updates
-                    }
-                    if (this._selectKeys) {
-                        options.select = this._selectKeys;
-                    }
-                    this.emit('BeforePatchOne', options, args);
                     repo.updateOne(options, (err, doc) => {
                         if (err) {
                             return res.sendRsp(err.code, err.message);
@@ -494,7 +492,7 @@ class EntityController extends ControllerBase {
                             method: 'patchOne',
                             data: doc
                         }, () => {
-                            this.emit('AfterPatchOne', doc);
+                            this._afterPatchOne(doc);
                             return res.sendSuccess(doc);
                         });
                     });
