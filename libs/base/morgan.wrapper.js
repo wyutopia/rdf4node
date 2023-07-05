61dd9169a5077b845393bbcf023b6ac5b3d64861
Eric Wang <wyutopia@hotmail.com>
2023-07-05 11:37:02 +0800

    Add ACCESS_LOG_ALL env variable to control access log

--- libs/base/morgan.wrapper.js
+++ libs/base/morgan.wrapper.js
@@ -11,17 +11,20 @@
      return tools.getClientIp(req);
  });
  
- let appName = process.env.SRV_ROLE || 'app';
- let logDir = process.env.LOG_DIR || path.join(appRootPath.path, 'logs');
+ const appName = process.env.SRV_ROLE || 'app';
+ const LOG_DIR = process.env.LOG_DIR || path.join(appRootPath.path, 'logs');
+ const QUITE_LOG = (process.env.NODE_ENV === 'production' && process.env.ACCESS_LOG_ALL === undefined);
+
  morgan.format(appName, ':xforwarded - [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent"');
  
  function MorganWrapper(proc) {
-     let accessLogStream = fs.createWriteStream(path.join(logDir, `${proc}-access.log`), {flags: 'a'});
-     let options = {};
-     if (process.env.NODE_ENV === 'production') {
-         options = {
-             skip: function (req, res) { return res.statusCode < 400 },
-             stream: accessLogStream
+     let accessLogStream = fs.createWriteStream(path.join(LOG_DIR, `${proc}-access.log`), {flags: 'a'});
+     let options = {
+        stream: accessLogStream
+     };
+     if (QUITE_LOG) {
+         options.skip = function (req, res) {
+            return res.statusCode < 400;
          };
      }
      return morgan(appName, options);
