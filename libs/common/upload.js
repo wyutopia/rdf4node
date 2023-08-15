1b11ea2d246dfbb7989ca1960f3c6a04341a16f9
Eric Wang <wyutopia@hotmail.com>
2023-08-15 09:56:44 +0800

    Add some libs into upload module

--- libs/common/upload.js
+++ libs/common/upload.js
@@ -1,10 +1,14 @@
 /**
  * Created by Eric on 2023/07/05
  */
+const AliOSS = require('ali-oss');
 const appRoot = require('app-root-path');
+const moment = require('moment');
 const multer = require('multer');
 const path = require('path');
 const tools = require('../../utils/tools');
+const {upload: config} = require('../../include/config');
+//
 const _DEFAULT_DIR = process.env.UPLOAD_DIR || path.join(appRoot.path, 'public/uploads/');
 console.log('>>>>>> The default upload dir: ', _DEFAULT_DIR);
 
@@ -19,4 +23,5 @@ function getUpload (absDir, options) {
     return _uploads[key];
 }
 
+// Define 
 module.exports = exports = getUpload;
